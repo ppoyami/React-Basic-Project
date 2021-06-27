18be1ae422882cae7efe1a2bd064a3c7d3209d61
intae kim <intaemon@intaeui-MacBookAir.local>
2021-06-27 13:48:22 +0900

    fix: a tag -> span

--- advenced-todo-list/src/components/Landing/LoginButton.js
+++ advenced-todo-list/src/components/Landing/LoginButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = styled.a`
+const Button = styled.span`
   padding: 1rem 3rem;
   background-color: ${({ theme: { colors } }) => colors.color_text};
   color: #fff;
